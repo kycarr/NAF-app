a864e9d665aacfbb81c0426348342fb490c7af5d
Yubo Wang <yubowang@usc.edu>
2018-06-12 05:44:35 -0700

    fetch update

--- naf_server_files/controllers/fetch_answers_controller.js
+++ naf_server_files/controllers/fetch_answers_controller.js
@@ -13,7 +13,18 @@ const Item = require('../models/Item');
 
 
 exports.fetchStudentAnswers = async (req,res) => {
-    const items = await Item.find();
+    var user = mongoose.Types.ObjectId(req.query['user_id']);
+    console.log(user); 
+    const items_list = await Section.find({user: user}, {items: 1});
+    let items = [];
+    for(let j = 0; j < items_list.length; j++) {
+      let items_id = items_list[j].items;
+      for(let k = 0; k < items_id.length; k++) {
+          const item = await Item.findOne({'_id': items_id[k]});
+          console.log(item);
+          items.push(item);
+      }
+    }
       const answerResponse = {};
       for(let i=0; i<items.length;i++) {
 
@@ -33,35 +44,3 @@ exports.fetchStudentAnswers = async (req,res) => {
       console.log(answerResponse);
       res.json(answerResponse);
 }
-
-
-/*
-
-
-  var user = mongoose.Types.ObjectId(req.query['user_id']);
-  console.log(user);
-    var answerResponse = {};
-    console.log(user_id); 
-    var item_ids = await Section.find({ 'user': user });
-    console.log(items_ids);
-    for(let k = 0; k < item_ids.length; k++) {
-        let item_id_list = item_ids[k];
-        for(let j = 0; j < item_id_list.length; j++) {
-            console.log(item_id_list[j]);
-                let currentAnswer = await Answer.find({item: item_id_list[j].item_id});
-                
-               if(currentAnswer === undefined || currentAnswer.length === 0) {
-                  console.log('no answer provided');
-                  currentAnswer ='' ;
-                } else {
-                  currentAnswer = currentAnswer[0].answers;
-                }
-
-               let correctAnswer = currentItem.correctAnswer;
-               answerResponse[j] = [currentAnswer, correctAnswer];
-        }
-    }
-  console.log(answerResponse);
-  res.json(answerResponse);
-
- */
