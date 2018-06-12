22516f199547e4a364370b08aab2cf50ea66ca16
Yubo Wang <yubowang@usc.edu>
2018-06-12 05:44:17 -0700

    fetch update with userid

--- report-server/test/fetchStudentAnswer_test.js
+++ report-server/test/fetchStudentAnswer_test.js
@@ -12,7 +12,7 @@ chai.request('http://localhost:8888')
 
   chai.request('http://localhost:8888')
   .get('/student/fetchStudentAnswers')
-  .query({user_id: "5b1efaad2c8243159b67c80c"})
+  .query({user_id: "5b1fb6c3c026e269b612b4d3"})
   .then(responses => {
     console.log(responses.text);
   });
