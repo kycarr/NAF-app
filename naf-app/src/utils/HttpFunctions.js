bcb3fa811ddda1d4f274dda3dcb3ef2492748c0e
Yubo Wang <yubowang@naf01.ict.usc.edu>
2018-06-13 12:21:06 -0700

    from get to post on save answer

--- naf-app/src/utils/HttpFunctions.js
+++ naf-app/src/utils/HttpFunctions.js
@@ -2,26 +2,24 @@ import axios from 'axios';
 
 
 export function login(username, password) {
-	return axios.post('http://localhost:8888/api/login', {
+	return axios.post('http://localhost:8080/api/login', {
          username,
          password
 	});
 }
 
 export function getQuestions(userId) {
-	return axios.post('http://localhost:8888/api/questions', {
+	return axios.post('http://localhost:8080/api/questions', {
 		 userId
 	});
 }
 
 export function saveAnswer(userId, sectionId, questionId, answer) {
-	return axios.get('http://localhost:8888/api/questionAnswerStore', {
-		params: {
-			userId: userId,
-			sectionId: sectionId,
-			questionId: questionId,
-			answer: answer
-		}
+	return axios.post('http://localhost:8080/api/questionAnswerStore', {
+			userId,
+			sectionId,
+			questionId,
+			answer
 	});
 }
 
@@ -29,7 +27,7 @@ export function saveResponse(userId, sectionId, questionId, response) {
 	  console.log("userID: " + userId);
   	  console.log("question ID: " + questionId);
   	  console.log("sectionId: " + sectionId);
-	return axios.post('http://localhost:8888/api/questionResponseStore', {
+	return axios.post('http://localhost:8080/api/questionResponseStore', {
 		userId,
 		sectionId,
 		questionId,
@@ -38,7 +36,7 @@ export function saveResponse(userId, sectionId, questionId, response) {
 }
 
 export function submitAnswer(userId, sectionId, timeLeft) {
-	return axios.post('http://localhost:8888/api/submitSection', {
+	return axios.post('http://localhost:8080/api/submitSection', {
 		userId,
 		sectionId,
 		timeLeft
@@ -46,7 +44,7 @@ export function submitAnswer(userId, sectionId, timeLeft) {
 }
 
 export function finishTest(userId) {
-	return axios.post('http://localhost:8888/api/finishTest', {
+	return axios.post('http://localhost:8080/api/finishTest', {
 		userId
 	});
 }
